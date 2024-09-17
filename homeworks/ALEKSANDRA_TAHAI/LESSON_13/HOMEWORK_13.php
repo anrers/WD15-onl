1caacd081b5373afe9853067ee922f0cd1a404cc
Aleksandra Tahai <alexsandra.tagay@gmail.com>
2024-09-17 20:10:02 +0300

    lesson-13 Aleksandra Tahai

---
+++ homeworks/ALEKSANDRA_TAHAI/LESSON_13/HOMEWORK_13.php
@@ -0,0 +1,37 @@
+<?php
+//1 task
+$x = 5; //объявление переменной
+echo($x + 3) .PHP_EOL; //увеличение значения на 3 и вывод результата + перенос строчки
+
+//2 task
+$a = 10;
+$b = 7; //объявление 2-х  переменных
+echo($a / $b) .PHP_EOL; //деление a на b и вывод результата
+
+//3 task
+$age=25;
+$name="Алиса"; //объявление 2-х  переменных
+echo "Меня зовут $name, и мне $age лет" .PHP_EOL; //вывод текста с переменными
+
+//4 task
+$y=12;
+echo ($y*2) .PHP_EOL;
+
+//5 task
+$x=7;
+$y=3;
+echo ($x*$y) .PHP_EOL;
+
+//6 task
+$z=3;
+$p=7;
+echo ($z+$p) .PHP_EOL;
+
+//7 task
+$firstString="do you know?";
+$sumString=$firstString. "мне нравится программирование";
+echo $sumString .PHP_EOL;
+
+//8 task
+$element=98;
+echo ++$element .PHP_EOL;
