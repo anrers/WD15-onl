ee9fe105132f0407e3d878462edba93a5b1ab969
Alrxandr <alexandr9652764@gmail.com>
2024-10-05 16:21:28 +0500

    homework: lesson-17 Alexandr Kobiliatskiy

--- homeworks/Alexandr_Kobiliatskiy/lesson_17/index_17.php
+++ homeworks/Alexandr_Kobiliatskiy/lesson_17/index_17.php
@@ -10,51 +10,56 @@
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="container-fluid bg-primary-subtle">
-    <?php
-    echo "1.Напишите PHP цикл, который выводит числа от 1 до 100 <br>";
-    for ($i = 1; $i <= 100; $i++) {
-        echo "$i; ";
-    };
-    echo "<br>";
-    echo "<br>";
-
-    echo "2.Напишите PHP цикл, который выводит ненумерованный список из 10 пунктов. <br>";
-    for ($i = 1; $i <= 10; $i++) {
-        echo "<li>Пункт $i</li>";
-    };
-    echo "<br>";
-    echo "<br>";
-
-    echo "3.Создайте массив из 100 случайных чисел. <br>";
-    $arr = [];
-    for ($i = 1; $i <= 100; $i++) {
-        $arr[] = mt_rand();
-    }
-    //print_r($arr);
-    echo "<br>";
-    echo "<br>";
-
-    echo "4.Вывести массив из предыдущего задания, при помощи цикла while, а потом при помощи foreach.<br>";
-    echo "Цикл while <br>";
-    $i = 0;
-    while ($i < 100) {
-        echo "$arr[$i]; ";
-        $i++;
-    }
-    echo "<br>";
-    echo "Цикл foreach <br>";
-    foreach ($arr as $i => $val) {
-        echo "$val; ";
-    }
-    echo "<br>";
-    echo "<br>";
+    <div class="container-fluid bg-primary-subtle">
+        <h3 class=" text-center">1. Напишите PHP цикл, который выводит числа от 1 до 100</h3>
+        <?php
+        for ($i = 1; $i <= 100; $i++) {
+            echo "$i; ";
+        };
     ?>
+    </div>
+
+    <div class="container-fluid bg-body-secondary">
+        <h3 class=" text-center">2. Напишите PHP цикл, который выводит ненумерованный список из 10 пунктов.</h3>
+        <ul>
+            <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<li>Пункт $i</li>";
+                };
+            ?>
+        </ul>
+    </div>
+
+    <div class="container-fluid bg-warning-subtle">
+    <h3 class="text-center">3. Создайте массив из 100 случайных чисел.</h3>
+    <?php 
+        $arr = [];
+        for ($i = 1; $i <= 100; $i++) {
+            $arr[] = mt_rand();
+        }
+    ?>
+    </div>
+
+    <div class="container-fluid bg-dark-subtle">
+        <h3 class="text-center">4. Вывести массив из предыдущего задания, при помощи цикла while, а потом при помощи foreach.</h3>
+        <?php
+            echo "Цикл while <br>";
+            $i = 0;
+            while ($i < 100) {
+                echo "$arr[$i]; ";
+                $i++;
+            }
+            echo "<br>";
+            echo "Цикл foreach <br>";
+            foreach ($arr as $i => $val) {
+                echo "$val; ";
+            }
+        ?>
+    </div>
+ 
 </div>
 <div class="container-fluid bg-success-subtle">
-    <?php
-    echo "5. Создайте массив из 10 строк и выведите их любым циклом внутри HTML-элемента div.";
-    ?>
+    <h3 class="text-center">5. Создайте массив из 10 строк и выведите их любым циклом внутри HTML-элемента div.</h3>
     <div class="container bg-info-subtle pb-2">
         <?php
         $strArr = ['Каждый', 'студент', 'должен', 'взять', 'любые', 'две', 'функции,', 'связанные', 'с', 'массивами',];
@@ -64,10 +69,12 @@
         ?>
     </div>
 </div>
-<div class="container-fluid bg-body-secondary">
+
+<div class="container-fluid bg-body-secondary pb-4">
+    
+    <h3 class="text-center">6. Создайте массив, каждый элемент которого тоже массив с ключами title, description, price. Выведите все элементы этого массива, так, чтобы заголовки были в HTML-элементе h2, описания в p, а цена в гиперсылке.</h3>
     <?php
-    echo "6. Создайте массив, каждый элемент которого тоже массив с ключами title, description, price. Выведите все элементы этого массива, так, чтобы заголовки были в HTML-элементе h2, описания в p, а цена в гиперсылке.";
-    $productArr = [
+        $productArr = [
         ['title' => 'Cadillac', 'description' => 'Life. Liberty. And the Pursuit', 'price' => 19000000],
         ['title' => 'Volkswagen', 'description' => 'Das Auto', 'price' => 8000000],
         ['title' => 'BMW', 'description' => 'Sheer Driving Pleasure', 'price' => 21000000],
@@ -76,56 +83,41 @@
     ?>
     <div class="container bg-success-subtle">
         <?php
-        foreach ($productArr as $key => $value) {
-            foreach ($value as $k => $v) {
-                if ($k == 'title') {
-                    echo "<h2>$v</h2>";
-                }
-                if ($k == 'description') {
-                    echo "<p>$v</p>";
-                }
-                if ($k == 'price') {
-                    echo "<a>$v</a><br>";
-                    echo "<br>";
-                }
-            }
-            echo "<br>";
+        foreach ($productArr as $value) {
+            $arrVal = array_values($value);
+            ?>
+            <h2 class="mb-0"><?=$arrVal[0]?></h2>
+            <p class="mb-0"><?=$arrVal[1]?></p>
+            <a class="link-offset-3 mb-0" href="#"><?=$arrVal[2]?></a>
+        <?php
         }
         ?>
     </div>
 </div>
 
 <div class="container-fluid bg-info">
-    <?php
-    echo "7. При выводе элементов из предыдущего задания покрасьте фон элементов ниже определенной цены в отличный от других цвет.";
-    ?>
-
+    <h3 class="text-center">7. При выводе элементов из предыдущего задания покрасьте фон элементов ниже определенной цены в отличный от других цвет.</h3>
     <div class="container bg-success-subtle">
         <?php
-        foreach ($productArr as $key => $value) {
-            foreach ($value as $k => $v) {
+            foreach ($productArr as $value) {
                 $value['price'] < 2000000 ? $colorBeggar = 'red' : $colorBeggar = 'green';
-                if ($k == 'title') {
-                    echo "<div style='background-color: $colorBeggar'><h2 style='color: white'>$v</h2></div>";
+                $arrVal = array_values($value);
+                ?>
+                <div class="container text-white" style='background-color: <?=$colorBeggar?>'>
+                    <h2 class="mb-0"><?=$arrVal[0]?></h2>
+                    <p class="mb-0"><?=$arrVal[1]?></p>
+                    <a class="link-offset-3 mb-0 text-white" href="#"><?=$arrVal[2]?></a>
+                </div>
+                <br>   
+                <?php
                 }
-                if ($k == 'description') {
-                    echo "<div style='background-color: $colorBeggar'><p style='color: white'>$v</p></div>";
-                }
-                if ($k == 'price') {
-                    echo "<div style='background-color: $colorBeggar'><a style='color: white'>$v</a></div><br>";
-                    echo "<br>";
-                }
-            }
-            echo "<br>";
-        }
-        ?>
+                ?>
     </div>
 </div>
 
 
-<?php
-echo "8.Создайте масcив из 50 случайных чисел от 0 до 100. Найти все числа меньшие 72 и поместить их в отдельный массив";
-?>
+<h3 class="text-center">8. Создайте масcив из 50 случайных чисел от 0 до 100. Найти все числа меньшие 72 и поместить их в отдельный массив</h3>
+
 <div class="container-fluid bg-danger-subtle">
     <?php
     $randArrNum = [];
@@ -133,11 +125,13 @@ echo "8.Создайте масcив из 50 случайных чисел от 
     for ($i = 0; $i < 50; $i++) {
         $randArrNum[] = mt_rand(0, 100);
     }
-    for ($i = 0; $i < count($randArrNum); $i++) {
-        if ($randArrNum[$i] < 72) {
-            $randArrNumFilter72[] = $randArrNum[$i];
+
+    foreach($randArrNum as $arrVal72){
+        if ($arrVal72 < 72) {
+            $randArrNumFilter72[] = $arrVal72;
         }
     }
+
     //        var_dump($randArrNum);
     //        echo "**********************************************";
     //        var_dump($randArrNumFilter72);
@@ -146,16 +140,15 @@ echo "8.Создайте масcив из 50 случайных чисел от 
 
 
 <div class="container-fluid bg-warning">
-    <?php
-    echo "9.Создайте цикл, который выводит числа то 0 до 100 в HTML-элементах div; окраска HTML-элементов должна чередоваться («зебра»).";
-    ?>
+
+    <h3 class="text-center">9. Создайте цикл, который выводит числа то 0 до 100 в HTML-элементах div; окраска HTML-элементов должна чередоваться («зебра»).</h3>
+
     <div class="container bg-dark-subtle">
         <?php
         for ($i = 0; $i < 101; $i++) {
             $i % 2 == 0 ? $colorZebra = 'grey' : $colorZebra = 'white';
             echo "<div style='background-color: $colorZebra'>$i</div>";
         }
-
         ?>
     </div>
 
@@ -167,7 +160,3 @@ echo "8.Создайте масcив из 50 случайных чисел от 
         crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
-
