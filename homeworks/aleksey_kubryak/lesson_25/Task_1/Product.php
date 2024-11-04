6e4b6ecc881c4c2d2f57c5ded162869e0e457a82
AKubryak <96373690+AKubryak@users.noreply.github.com>
2024-11-04 17:53:44 +0300

    homework: lesson-25 Aleksey Kubryak (#125)

---
+++ homeworks/aleksey_kubryak/lesson_25/Task_1/Product.php
@@ -0,0 +1,52 @@
+<?php
+
+class Product 
+{
+    public function __construct(
+        public string $name,
+        public float $price,
+        public string $description,
+        public int $quantity,
+    ) {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+}
---
+++ homeworks/aleksey_kubryak/lesson_25/Task_2/Book.php
@@ -0,0 +1,26 @@
+<?php
+
+class Book 
+{
+    public function __construct(
+        public string $title,
+        public int $author_id,
+        public bool $is_available,
+    ) {
+    }
+
+    public function getTitle(): string 
+    {
+        return $this->title;
+    }
+
+    public function isAvailable(): bool 
+    {
+        return $this->is_available;
+    }
+
+    public function getAuthorId(): int 
+    {
+        return $this->author_id;
+    }
+}
