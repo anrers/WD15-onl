556363870ada1dc6e4f2fe955f6dd83f4d4473dc
Kobiliatskiy Alexandr <alexandr9652764@gmail.com>
2024-11-15 13:02:05 +0500

    homework: lesson-27 Alexandr Kobiliatskiy

--- homeworks/Alexandr_Kobiliatskiy/lesson_27/logic.php
+++ homeworks/Alexandr_Kobiliatskiy/lesson_27/logic.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
-if (!empty($_POST['userName']) and !empty($_POST['userAge']) and !empty($_POST['userEmail'])) {
-    if (preg_match("/^[a-zA-Z ]*$/", $_POST['userName']) == 1 and preg_match("/^[0-9]*$/", $_POST['userAge']) == 1) {
 
+$сondition1 = (!empty($_POST['userName']) and !empty($_POST['userAge']) and !empty($_POST['userEmail']));
+$сondition2 = (preg_match("/^[a-zA-Z ]*$/", $_POST['userName']) == 1 and preg_match("/^[0-9]*$/", $_POST['userAge']) == 1);
+
+if ($сondition1 and $сondition2) {
         $_SESSION['result'] = 1;
 
         $connection = new mysqli (
@@ -11,7 +13,6 @@ if (!empty($_POST['userName']) and !empty($_POST['userAge']) and !empty($_POST['
             password: '',
             database: 'study'
         );
-
         $userName = $_POST['userName'];
         $userAge = (int)$_POST['userAge'];
         $userEmail = $_POST['userEmail'];
@@ -19,26 +20,10 @@ if (!empty($_POST['userName']) and !empty($_POST['userAge']) and !empty($_POST['
         $sql = "INSERT INTO users (name, age, email) VALUE ('$userName', $userAge, '$userEmail')";
         $result = $connection->query($sql);
 
-
-        //Часть задания которая после добавления пользователя из формы
-        $selectUser = "SELECT name FROM users WHERE id=9";
-        $resultSelectUser = $connection->query($selectUser);
-        
-        $changeUsedata = "UPDATE users SET name='Semen', age=20, email='semen@mail.ru' WHERE id=10";
-        $resultchangeUsedata = $connection->query($changeUsedata);
-
-        $deleteUser = "DELETE FROM users WHERE id=11";
-        $resultDeleteUser = $connection->query($deleteUser);
-
         $connection->close();
 
-       } else  {
-        $_SESSION['result'] = 0;
-        header("location: https://homework-27/index.php");
-        die;
-       } 
 } else {
     $_SESSION['result'] = 0;
-    header("location: https://homework-27/index.php");
 }
-?>
+
+header("location: https://homework-lesson-27/index.php");
