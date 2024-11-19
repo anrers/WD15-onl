4de4869df961fda4ca99df8e0ba4afced41b50a2
Alrxandr <alexandr9652764@gmail.com>
2024-11-19 21:15:16 +0500

    homework: lesson-28 Alexandr Kobiliatskiy

--- homeworks/Alexandr_Kobiliatskiy/lesson_28/basics/addEnrollments.php
+++ homeworks/Alexandr_Kobiliatskiy/lesson_28/basics/addEnrollments.php
@@ -4,58 +4,40 @@ if ($conn->connect_error){
     die ("Ошибка: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name FROM students";
+$sql = "SELECT students.id, students.name, students.email FROM students LEFT JOIN enrollments ON students.id = enrollments.student_id WHERE enrollments.student_id IS NULL";
 $result = $conn->query($sql);
 
-$studentIdAll = [];
-foreach ($result as $row) {
-    $studentIdAll[] = $row['id'];
-}
-
-$sqiSelectEnrollments = "SELECT student_id FROM enrollments";
-$result2 = $conn->query($sqiSelectEnrollments);
-
-$addedStudents = [];
-foreach ($result2 as $row) {
-    $addedStudents[] = $row['student_id'];
-}
-
-$notAddedStudents = array_diff($studentIdAll, $addedStudents);
+$count = count($result->fetch_all());
 
 $h1 = "Студентов не занесенных в таблицу enrollments не найдено";
-if (count($notAddedStudents) > 1) {
+if ($count > 1) {
     $h1 = "Эти студенты не занесены в таблицу enrollments";
 }
 
-if (count($notAddedStudents) == 1) {
+if ($count  == 1) {
     $h1 = "Этот студент не занесен в таблицу enrollments";
 }
 
-//var_dump($notAddedStudents);
 echo "<h1>$h1</h1>";
 echo "<table><tr><th>Id</th><th>Имя</th></tr>";
 foreach ($result as $row) {
-    if (in_array($row['id'], $notAddedStudents)) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["name"] . "</td>";
-        echo "</tr>";
-    }
+    echo "<tr>";
+    echo "<td>" . $row["id"] . "</td>";
+    echo "<td>" . $row["name"] . "</td>";
+    echo "</tr>";
 }
 echo "</table>"; ?>
 
-<form action="" method="post">
-    <input name="addStudents" type="submit" value="Добавить">
-</form>
+    <form action="" method="post">
+        <input name="addStudents" type="submit" value="Добавить">
+    </form>
 
 <?php
 
 if (isset($_POST['addStudents'])) {
     foreach ($result as $row) {
-        if (in_array($row['id'], $notAddedStudents)) {
-            $studId = $row['id'];
-            $result3 = $conn->query("INSERT INTO enrollments (student_id) VALUES ('$studId')");
-        }
+        $studId = $row['id'];
+        $result3 = $conn->query("INSERT INTO enrollments (student_id) VALUES ('$studId')");
     }
 }
 
