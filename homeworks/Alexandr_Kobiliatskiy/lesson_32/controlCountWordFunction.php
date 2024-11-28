2c89d7dbe0eee8f34342434d4603a11496e20caa
Alexandr23111978 <102842662+Alexandr23111978@users.noreply.github.com>
2024-11-28 19:07:58 +0500

    homework-32 Alexandr Kobiliatskiy (#159)

---
+++ homeworks/Alexandr_Kobiliatskiy/lesson_32/controlCountWordFunction.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'countWordFrequency.php';
+
+var_dump(countWordFrequency("
+Thank you for your letter! I will answer your questions about my family with pleasure. 
+Today is Sunday, so we are all together. It is raining, that's why we are all sitting in the living room. 
+My Mom is reading. She is a teacher of literature, so she reads a lot. She is a slim woman of about 40. 
+Her pupils like her for she is an going and a kind person. Reading is her hobby. Besides, she is fond of cooking. 
+When
+we gather together after a working day, there is always something tasty on the table. My father helps her sometimes 
+with the house work. He is a doctor and has very little free time. He works a lot. When I was a child, I often visited 
+him at the hospital and saw him at work. He always listens attentively to his patients and gives them his advice. 
+His patients trust him and that makes his work easier. He decided to become a doctor, because his father, 
+my grandfather, was a doctor too. They look very much alike. All my grandparents are retired now. They all live in 
+the village, where my parents come from. They are happy to see the entire family when we come to them. We have many 
+relatives. My aunts, uncles, and cousins live in different parts of Ukraine. On holidays we often gather together and 
+have a very good time!
+"));
