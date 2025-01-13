6a6456d6a63807107217f001f212a94ab17fe321
Alexandr23111978 <102842662+Alexandr23111978@users.noreply.github.com>
2025-01-13 15:59:32 +0300

    homework: lesson 39 Alexandr kobiliatskiy (#188)
    * homework:lesson-39 Alexandr Kobiliatskiy before execution starts HW-39
    
    * homework: lesson-39 point-1 Alexandr Kobiliatskiy
    
    * homework: lesson-39 point-2,3 Alexandr Kobiliatskiy
    
    * homework: lesson-39 Alexandr Kobiliatskiy
    
    * homework: lesson-39 Alexandr Kobiliatskiy

---
+++ homeworks/Alexandr_Kobiliatskiy/laravel/database/factories/Tasks/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\Tasks;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tasks\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(10),
+            'dueDate' => $this->faker->dateTimeBetween('now', '+1 years'),
+        ];
+    }
+}
---
+++ homeworks/Alexandr_Kobiliatskiy/laravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Tasks\TaskController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+$var = UserController::class;
+Route::get('/home', [UserController::class, 'home']);
+
+$var2 = MainController::class;
+Route::get('/mai', [MainController::class, 'mai']);
+
+$var3 = TaskController::class;
+Route::get('/tasks', [TaskController::class, 'getAll']);
+
