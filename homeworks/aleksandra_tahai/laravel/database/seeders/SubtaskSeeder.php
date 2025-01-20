2a01d7a948fe4f9243515a074a4a43d527523e00
alxsndraa <116572214+AleksandraTahai@users.noreply.github.com>
2025-01-20 19:37:31 +0300

    homework: lesson-39 Aleksandra Tahai (#201)

---
+++ homeworks/aleksandra_tahai/laravel/database/seeders/SubtaskSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tasks\Subtask;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubtaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subtask = new Subtask();
+        $subtask->task_id = 1;
+        $subtask->name = 'Subtask 1';
+        $subtask->save();
+    }
+}
