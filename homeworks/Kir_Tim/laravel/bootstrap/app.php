a8a37c415aeaa45be6989507e0c50181fd880070
Kir Tim <kirilltimtk@gmail.com>
2025-01-26 17:51:21 +0300

    homework: lesson 42 Kirill Tim

--- homeworks/Kir_Tim/laravel/bootstrap/app.php
+++ homeworks/Kir_Tim/laravel/bootstrap/app.php
@@ -13,13 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('web', [
             \App\Http\Middleware\HostValidation::class,
-            # \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            # \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            # \Illuminate\Session\Middleware\StartSession::class,
-            # \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            # \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-            # \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            # // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
         ]);
 
         $middleware->group('api', [
